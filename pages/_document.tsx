3a2b0b5c3cc062f0affba116c86d530b4da4c019
herbowicz <herbowicz@gmail.com>
2023-05-14 18:36:25 +0200

    pwa

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -3,7 +3,11 @@ import { Html, Head, Main, NextScript } from 'next/document'
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+      <link rel="manifest" href="/manifest.json" />
+        <link rel="apple-touch-icon" href="/favicon.ico"></link>
+        <meta name="theme-color" content="#161916" />
+      </Head>
       <body>
         <Main />
         <NextScript />
