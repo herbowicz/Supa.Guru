16b0b3c3d7dcedfa253624560bb8ef78882b1aa9
herbowicz <herbowicz@gmail.com>
2023-05-14 15:13:40 +0200

    select avatar

--- components/selectAvatar.js
+++ components/selectAvatar.js
@@ -4,27 +4,28 @@ import { useUser } from '../context/user'
 
 export default function SelectAvatar() {
     const { user } = useUser()
+    const [i, setI] = useState(1)
     
     return (
         <div className='flex flex-col bg-green-100/1 p-5 text-center'>
-            <Image className={`mx-auto`} src='https://robohash.org/autquiaut.png?size=250x250&set=set2' alt='robohash' width='250' height='250' />
+            <Image className={`mx-auto`} src={`https://robohash.org/autquiaut.png?size=250x250&set=set${i}`} alt='robohash' width='250' height='250' />
 
             <p className={`p-2`}>
-                <span className={`text-2xl`}>{user?.email}</span>
+                <span className={`text-3xl`}>{user?.email}</span>
                 <br />
-                <span className={`text-xl`}>Select your Avatar</span>
+                <span className={`text-xl font-thin`}>Select your Avatar</span>
             </p>
 
             <p>
                 {new Array(5).fill().map((el, i) => (
                     <Image key='i' className={`p-4 inline-block`} alt='robohash' width='100' height='100'
-                        src={`https://robohash.org/autquiaut.png?size=50x50&set=set${i + 1}`} />
+                        src={`https://robohash.org/autquiaut.png?size=50x50&set=set${i + 1}`} onClick={() => setI(i + 1)} />
                 ))}
             </p>
 
-            <p className={`mt-3 text-xs overflow-hidden text-orange-600`}>
+            {/* <p className={`mt-3 text-xs overflow-hidden text-violet-900`}>
                 {JSON.stringify(user, null, 2)}
-            </p>
+            </p> */}
         </div>
     )
 }
