5411423f8669833f236f2b7712e71d94b2b88f82
herbowicz <herbowicz@gmail.com>
2023-05-14 15:21:23 +0200

    no line

--- components/nav.js
+++ components/nav.js
@@ -5,7 +5,7 @@ const Nav = () => {
     const { user } = useUser()
 
     return (
-        <nav className='flex max-w-xl mx-auto justify-center gap-5 py-4 px-6 border-b border-[color:var(--main)] text-[color:var(--main)]'>
+        <nav className='flex max-w-xl mx-auto justify-center gap-5 py-4 px-6 text-[color:var(--main)]'>
             {user && <div>{user?.points}</div>}
             <Link href='/'>Home</Link>
             <Link href='/shop'>Shop</Link>
